12cb26b2784f9754751c048a20226d5c0da81b68
Nadheva <nadheva@bawaslu.go.id>
2026-04-24 17:40:11 +0700

    fix: memperbaiki test RegisterUser agar CI kembali hijau

--- src/Domains/users/entities/_test/RegisterUser.test.js
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -69,7 +69,7 @@ describe("a RegisterUser entities", () => {
     const { username, fullname, password } = new RegisterUser(payload);
 
     // Assert
-    expect(username).toEqual("sengaja-disalahkan");
+    expect(username).toEqual(payload.username);
     expect(fullname).toEqual(payload.fullname);
     expect(password).toEqual(payload.password);
   });
